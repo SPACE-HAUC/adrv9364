9c47424f6d7c370ace7b43704c5f8f61053316c6
npsacco <Nicholas_Sacco@student.uml.edu>
2019-12-13 18:34:37 -0500

    Pre-tx/rx test

--- ADRV.cpp
+++ ADRV.cpp
@@ -1,447 +1,220 @@
 
-#include <iostream>
 #include "ADRV.h"
 
-using std::cout;
-using std::endl;
-
-ADRV::ADRV()
+bool initADRV()
 {
-  ADRV_id = 1;
-  cout << "I AM IN DEFAULT" << endl;
-  ctx = iio_create_default_context();
-  physDev = iio_context_find_device(ctx, AD9361PHYS.c_str());
-  contextDeviceCount = iio_context_get_devices_count(ctx);
-
-  txConfig.bw_hz = 0;
-  txConfig.fs_hz = 0;
-  txConfig.lo_hz = 0;
-  txConfig.rfport = "NO SELECTION";
-
-  rxConfig.bw_hz = 0;
-  rxConfig.fs_hz = 0;
-  rxConfig.lo_hz = 0;
-  rxConfig.rfport = "NO SELECTION";
-
-  tx = NULL;
-  tx0_q = NULL;
-  tx0_i = NULL;
-  tx_phys = NULL;
-  tx_lo = NULL;
-  txBuf = NULL;
-
-  rx = NULL;
-  rx0_q = NULL;
-  rx0_i = NULL;
-  rx_phys = NULL;
-  rx_lo = NULL;
-  rxBuf = NULL;
-}
-
-ADRV::ADRV(gigaInt tx_bw_hz, gigaInt tx_fs_hz, gigaInt tx_lo_hz,
-    gigaInt rx_bw_hz, gigaInt rx_fs_hz, gigaInt rx_lo_hz,
-    const char* tx_portName, const char* rx_portName)
-{
-  ADRV_id = 2;
-  ctx = iio_create_default_context();
-  contextDeviceCount = iio_context_get_devices_count(ctx);
-  physDev = iio_context_find_device(ctx, AD9361PHYS.c_str());
-
-  txConfig.bw_hz = tx_bw_hz;
-  txConfig.fs_hz = tx_fs_hz;
-  txConfig.lo_hz = tx_lo_hz;
-  txConfig.rfport = tx_portName;
-
-  rxConfig.bw_hz = rx_bw_hz;
-  rxConfig.fs_hz = rx_fs_hz;
-  rxConfig.lo_hz = rx_lo_hz;
-  rxConfig.rfport = rx_portName;
-
-  tx = NULL;
-  tx0_q = NULL;
-  tx0_i = NULL;
-  tx_phys = NULL;
-  tx_lo = NULL;
-  txBuf = NULL;
-
-  rx = NULL;
-  rx0_q = NULL;
-  rx0_i = NULL;
-  rx_phys = NULL;
-  rx_lo = NULL;
-  rxBuf = NULL;
-}
-
-// Parameterized Constructor by structure
-ADRV::ADRV(struct stream_cfg& txCfg, struct stream_cfg& rxCfg)
-{
-  ADRV_id = 3;
-  ctx = iio_create_default_context();
-  contextDeviceCount = iio_context_get_devices_count(ctx);
-  physDev = iio_context_find_device(ctx, AD9361PHYS.c_str());
-
-  txConfig = txCfg;
-  rxConfig = rxCfg;
-
-  tx = NULL;
-  tx0_q = NULL;
-  tx0_i = NULL;
-  tx_phys = NULL;
-  tx_lo = NULL;
-  txBuf = NULL;
-
-  rx = NULL;
-  rx0_q = NULL;
-  rx0_i = NULL;
-  rx_phys = NULL;
-  rx_lo = NULL;
-  rxBuf = NULL;
-}
-//******************************************************************************
-ADRV::~ADRV()
-{
-  cout << "ADRV DEST CONST CALL" << endl;
-}
-
-void ADRV::printID()
-{
-  cout << "ID OF ADRV: " << ADRV_id << endl;
-}
-
-void ADRV::printObject()
-{
-  cout << "ADRV OBJECT" << endl;
-}
-
-void ADRV::contextHK()
-{
-  cout << "********** CONTEXT HOUSE KEEPING **********" << endl;
-  const char* name = iio_context_get_name(ctx);
-  const char* description = iio_context_get_description(ctx);
-  uint32_t attributeCount = iio_context_get_attrs_count(ctx);
-  uint32_t devCount = iio_context_get_devices_count(ctx);
-  cout << "Context name: " << name << endl;
-  cout << "Context description: " << description << endl;
-  cout << "Device count: " << devCount << endl;
-  cout << "Attribute count: " << attributeCount << endl;
-
-  const char** attrName, attrVal;
-  for (int i = 0; i < attributeCount; i++)
-  {
-    int ret = iio_context_get_attr(ctx, i, attrName, attrVal);
-    cout << "Attribute Name: " << attrName << " Attribute Value: " << attrVal << endl;
-  }
-
-  return;
-}
-
-void ADRV::deviceHK()
-{
-
-}
-
-void ADRV::channelHK()
-{
-
-}
-
-void ADRV::processError(errorCode error, bool debug)
-{
-  if (debug)
-  {
-    switch(error)
+    bool initFlag = false;
+    ctx = iio_create_default_context();
+    if (ctx != NULL)
     {
-      case NO_ERROR: break;
-      case DEV_ERROR: 
-        cout << "ERROR - DEVICE IN FAULT" << endl; 
-        break;
-      case CHN_ERROR:
-        cout << "ERROR - CHANNEL IN FAULT" << endl;
-        break;
-      case PHYS_ERROR:
-        cout << "ERROR - PHYSICAL DEVICE IN FAULT" << endl;
-        break;
-      case LO_ERROR:
-        cout << "ERROR - LOCAL OSCILLATOR IN FAULT" << endl;
-        break;
-      case BUF_ERROR:
-        cout << "ERROR - BUFFER IN FAULT" << endl;
-        break;
-      default:
-        cout << "UNSPECIFIED ERROR" << endl;
+        physDev = iio_context_find_device(ctx, AD9361PHYS.c_str());
+        if (physDev != NULL)
+        {
+            initFlag = true;
+            cout << "ADRV INITIALIZED" << endl;
+        }
     }
-  }
-  
+    return initFlag;
 }
 
-void ADRV::initializeTransmitter()
+
+bool initTX(static struct stream_cfg* transmitterConfig)
 {
-  // Create the transmitter device
-  tx = iio_context_find_device(ctx, TXDEVICE.c_str());
+    txConfig = *transmitterConfig;
+    // Create the transmitter device
+    tx = iio_context_find_device(ctx, TXDEVICE.c_str());
   
-  // Proceed if transmitter created successfully
-  if (tx != NULL)
-  {
-    cout << "***** Transmitter device initialized" << endl;
-    tx0_i = iio_device_find_channel(tx, "voltage0", TXMODE);
+    // Proceed if transmitter created successfully
+    if (tx != NULL)
+    {
+        cout << "***** Transmitter device initialized" << endl;
+        tx0_i = iio_device_find_channel(tx, "voltage0", TXMODE);
    
-    if (tx0_i == NULL)
-    {
-      cout << "COULD NOT FIND TX VOLTAGE0, USING ALTVOLTAGE" << endl;
-      tx0_i = iio_device_find_channel(tx, "altvoltage0", TXMODE);
-      if (tx0_i == NULL)
-      {
-        processError(CHN_ERROR, debugFlag);
-      }
+        if (tx0_i == NULL)
+        {
+            cout << "COULD NOT FIND TX VOLTAGE0, USING ALTVOLTAGE" << endl;
+            tx0_i = iio_device_find_channel(tx, "altvoltage0", TXMODE);
+            if (tx0_i == NULL)
+            {
+                processError(CHN_ERROR, debugFlag);
+            }
+        }
+        else
+        {
+            cout << "********** Transmitter device, i-channel initialized" << endl;
+        }
+    
+        tx0_q = iio_device_find_channel(tx, "voltage1", TXMODE);
+    
+        if (tx0_q == NULL)
+        {
+            cout << "COULD NOT FIND TX VOLTAGE1, USING ALTVOLTAGE" << endl;
+            tx0_q = iio_device_find_channel(tx, "altvoltage1", TXMODE);
+            if (tx0_q == NULL)
+            {
+                processError(CHN_ERROR, debugFlag);
+            }
+        }
+        else
+        {
+            cout << "********** Transmitter device, q-channel initialized" << endl;
+        }
     }
     else
     {
-       cout << "********** Transmitter device, i-channel initialized" << endl;
+        processError(DEV_ERROR, debugFlag);
     }
-    
-    tx0_q = iio_device_find_channel(tx, "voltage1", TXMODE);
-    
-    if (tx0_q == NULL)
+  
+    // Create the transmitter physical channel
+    tx_phys = iio_device_find_channel(physDev, "voltage0", true);
+  
+    if (tx_phys != NULL)
     {
-      cout << "COULD NOT FIND TX VOLTAGE1, USING ALTVOLTAGE" << endl;
-      tx0_q = iio_device_find_channel(tx, "altvoltage1", TXMODE);
-      if (tx0_q == NULL)
-      {
-        processError(CHN_ERROR, debugFlag);
-      }
+        cout << "********** Transmitter device, phys-channel initialized" << endl;
+        iio_channel_attr_write_longlong(tx_phys, ATTR_BW.c_str(), txConfig.bw_hz);
+        iio_channel_attr_write_longlong(tx_phys, ATTR_FS.c_str(), txConfig.fs_hz);
+        iio_channel_attr_write(tx_phys, ATTR_PORT.c_str(), txConfig.rfport);
     }
     else
     {
-      cout << "********** Transmitter device, q-channel initialized" << endl;
+        processError(PHYS_ERROR, debugFlag);
     }
-    
-  }
-  else
-  {
-    processError(DEV_ERROR, debugFlag);
-  }
-  
-  // Create the transmitter physical channel
-  tx_phys = iio_device_find_channel(physDev, "voltage0", true);
-  
-  if (tx_phys != NULL)
-  {
-    cout << "********** Transmitter device, phys-channel initialized" << endl;
-    iio_channel_attr_write_longlong(tx_phys, ATTR_BW.c_str(), txConfig.bw_hz);
-    iio_channel_attr_write_longlong(tx_phys, ATTR_FS.c_str(), txConfig.fs_hz);
-    iio_channel_attr_write(tx_phys, ATTR_PORT.c_str(), txConfig.rfport);
-  }
-  else
-  {
-    processError(PHYS_ERROR, debugFlag);
-  }
 
-  // Create the transmitter local oscillator
-  tx_lo = iio_device_find_channel(physDev, "altvoltage1", true);
-  if (tx_lo != NULL)
-  {
-    cout << "********** Transmitter device, lo-channel initialized" << endl;
-    iio_channel_attr_write_longlong(tx_lo, ATTR_F.c_str(), txConfig.lo_hz);
-  }
-  else
-  {
-    processError(LO_ERROR, debugFlag);
-  }
+    // Create the transmitter local oscillator
+    tx_lo = iio_device_find_channel(physDev, "altvoltage1", true);
+    if (tx_lo != NULL)
+    {
+        cout << "********** Transmitter device, lo-channel initialized" << endl;
+        iio_channel_attr_write_longlong(tx_lo, ATTR_F.c_str(), txConfig.lo_hz);
+    }
+    else
+    {
+        processError(LO_ERROR, debugFlag);
+    }
  
-  if (tx0_i != NULL && tx0_q != NULL)
-  {
-    iio_channel_enable(tx0_i);
-    iio_channel_enable(tx0_q);
-    cout << "********** Transmitter channels i and q enabled" << endl;
-  }
-  txBuf = iio_device_create_buffer(tx, MAX_BUFFER_SIZE, NONCYCLIC);
-  if (txBuf == NULL)
-  {
-    processError(BUF_ERROR, debugFlag);
-  }
-  else
-  {
-    cout << "********** Transmitter buffer initialized" << endl;
-  }
+    if (tx0_i != NULL && tx0_q != NULL)
+    {
+        iio_channel_enable(tx0_i);
+        iio_channel_enable(tx0_q);
+        cout << "********** Transmitter channels i and q enabled" << endl;
+    }
+    txBuf = iio_device_create_buffer(tx, MAX_BUFFER_SIZE, NONCYCLIC);
+    if (txBuf == NULL)
+    {
+        processError(BUF_ERROR, debugFlag);
+    }
+    else
+    {
+        cout << "********** Transmitter buffer initialized" << endl;
+    } 
 }
 
-void ADRV::initializeReceiver()
+bool initRX(static struct stream_cfg* receiverConfig)
 {
-  // Create the receiver device
-  rx = iio_context_find_device(ctx, RXDEVICE.c_str());
+    rxConfig = *receiverConfig;
+    // Create the receiver device
+    rx = iio_context_find_device(ctx, RXDEVICE.c_str());
 
-  if (rx != NULL)
-  {
-    cout << "***** Receiver device initialized" << endl;
-    rx0_i = iio_device_find_channel(rx, "voltage0", RXMODE);
-    if (rx0_i == NULL)
+    if (rx != NULL)
     {
-      cout << "COULD NOT FIND RX VOLTAGE0, USING ALTVOLTAGE" << endl;
-      rx0_i = iio_device_find_channel(rx, "altvoltage0", RXMODE);
-      if (rx0_i == NULL)
-      {
-        processError(CHN_ERROR, debugFlag);
-      }
+        cout << "***** Receiver device initialized" << endl;
+        rx0_i = iio_device_find_channel(rx, "voltage0", RXMODE);
+        if (rx0_i == NULL)
+        {
+            cout << "COULD NOT FIND RX VOLTAGE0, USING ALTVOLTAGE" << endl;
+            rx0_i = iio_device_find_channel(rx, "altvoltage0", RXMODE);
+            if (rx0_i == NULL)
+            {
+                processError(CHN_ERROR, debugFlag);
+            }
+        }
+        rx0_q = iio_device_find_channel(rx, "voltage1", RXMODE);
+        if (rx0_q == NULL)
+        {
+            cout << "COULD NOT FIND RX VOLTAGE1, USING ALTVOLTAGE" << endl;
+            rx0_q = iio_device_find_channel(rx, "altvoltage1", RXMODE);
+            if (rx0_q == NULL)
+            {
+                processError(CHN_ERROR, debugFlag);
+            }
+        }
     }
-    rx0_q = iio_device_find_channel(rx, "voltage1", RXMODE);
-    if (rx0_q == NULL)
+    else
     {
-      cout << "COULD NOT FIND RX VOLTAGE1, USING ALTVOLTAGE" << endl;
-      rx0_q = iio_device_find_channel(rx, "altvoltage1", RXMODE);
-      if (rx0_q == NULL)
-      {
-        processError(CHN_ERROR, debugFlag);
-      }
+        processError(DEV_ERROR, debugFlag);
     }
-  }
-  else
-  {
-    processError(DEV_ERROR, debugFlag);
-  }
 
-  // Create the receiver physical channel
-  rx_phys = iio_device_find_channel(physDev, "voltage0", false);
-  if (rx_phys != NULL)
-  {
-    iio_channel_attr_write_longlong(rx_phys, ATTR_BW.c_str(), rxConfig.bw_hz);
-    iio_channel_attr_write_longlong(rx_phys, ATTR_FS.c_str(), rxConfig.fs_hz);
-    iio_channel_attr_write(rx_phys, ATTR_PORT.c_str(), rxConfig.rfport);
-  }
-  else
-  {
-    processError(PHYS_ERROR, debugFlag);
-  }
+    // Create the receiver physical channel
+    rx_phys = iio_device_find_channel(physDev, "voltage0", false);
+    if (rx_phys != NULL)
+    {
+        iio_channel_attr_write_longlong(rx_phys, ATTR_BW.c_str(), rxConfig.bw_hz);
+        iio_channel_attr_write_longlong(rx_phys, ATTR_FS.c_str(), rxConfig.fs_hz);
+        iio_channel_attr_write(rx_phys, ATTR_PORT.c_str(), rxConfig.rfport);
+    }
+    else
+    {
+        processError(PHYS_ERROR, debugFlag);
+    }
 
-  // Create the receiver local oscillator
-  rx_lo = iio_device_find_channel(physDev, "altvoltage0", true);
-  if (rx_lo != NULL)
-  {
-    iio_channel_attr_write_longlong(rx_lo, ATTR_F.c_str(), rxConfig.lo_hz);
-  }
-  else
-  {
-    processError(LO_ERROR, debugFlag);
-  }
+    // Create the receiver local oscillator
+    rx_lo = iio_device_find_channel(physDev, "altvoltage0", true);
+    if (rx_lo != NULL)
+    {
+        iio_channel_attr_write_longlong(rx_lo, ATTR_F.c_str(), rxConfig.lo_hz);
+    }
+    else
+    {
+        processError(LO_ERROR, debugFlag);
+    }
 
-  if (rx0_i != NULL && rx0_q != NULL)
-  {
-    iio_channel_enable(rx0_i);
-    iio_channel_enable(rx0_q);
-    cout << "********** Receiver channels i and q enabled" << endl;
-  }
+    if (rx0_i != NULL && rx0_q != NULL)
+    {
+        iio_channel_enable(rx0_i);
+        iio_channel_enable(rx0_q);
+        cout << "********** Receiver channels i and q enabled" << endl;
+    }
     
-  rxBuf = iio_device_create_buffer(rx, MAX_BUFFER_SIZE, NONCYCLIC);
+    rxBuf = iio_device_create_buffer(rx, MAX_BUFFER_SIZE, NONCYCLIC);
 
-  if (rxBuf == NULL)
-  {
-    processError(BUF_ERROR, debugFlag);
-  }
+    if (rxBuf == NULL)
+    {
+        processError(BUF_ERROR, debugFlag);
+    }
 }
 
-/*void ADRV::initializeBuffer(struct iio_buffer* buffer, mode operation)
+bool createPacket(const char* strMsg, uint32_t numBytes, uint32_t** packet, uint32_t* numBytesRet)
 {
-  if (operation == TXMODE)
-  {
-    txBuf = buffer;
-  }
-  else if (operation == RXMODE)
-  {
-    rxBuf = buffer;
-  }
-}*/
+    uint32_t* byteArray = (uint32_t*)malloc(numBytes + HEADER_SIZE);
+    uint32_t byteArrayIndex = (numBytes + HEADER_SIZE) / sizeof(uint32_t);
 
-void ADRV::writePacketToBuffer(uint8_t* packet, uint32_t numBytes)
-{
-  /*uint8_t* p_dat = (uint8_t*)iio_buffer_first(txBuf, tx0_i);
-  cout << "PDAT SET" << endl;
-  //void *ptr = iio_buffer_first(txBuf, tx0_q);
-  for (int i = 0; i < numBytes; i++)
-  {
-    p_dat = packet[i];
-    p_dat += iio_buffer_step(txBuf);
-  }*/
-  /*char *p_dat, *p_end;
-	ptrdiff_t p_inc;
-  p_inc = iio_buffer_step(txBuf);
-  cout << "P INC: " << p_inc << endl;
-	p_end = iio_buffer_end(txBuf);
-  int i = 0;
-  for (p_dat = (char *)iio_buffer_first(txBuf, tx0_i); p_dat < p_end; p_dat += p_inc) 
-  {
-    *p_dat = packet[i];
-    i++;
-  }*/
-  //iio_buffer_set_data(txBuf, (void*)packet);
-  int i = 0;
-  cout << "WRITE: PACKET TO BUFFER" << endl;
-  for (uint8_t* ptr = (uint8_t*)iio_buffer_first(txBuf, tx0_i); ptr < iio_buffer_end(txBuf) && i < numBytes; ptr += iio_buffer_step(txBuf))
-  {
-    *ptr = packet[i];
-    i++;
-  }
-  cout << "WRITING COMPLETE" << endl;
-  //size_t bytesWritten = iio_channel_write(tx0_i, txBuf, packet, numBytes);
-  //cout << "MOVED " << bytesWritten << " BYTES OF DATA" << endl;
-  //std::memcpy(iio_buffer_start(txBuf), (void*)packet, numBytes);
+    cout << "NUM ELEMENTS: " << byteArrayIndex << endl;
+    cout << "CREATING PACKET..." << endl;
 
+    std::memcpy(&byteArray[0], &PACKET_START, 4]);
+    cout << "1" << endl;
+    std::memcpy(&byteArray[1], &numBytes, 4);
+    cout << "2" << endl;
+    std::memcpy(&byteArray[2], &MESSAGE_START, 4);
+    cout << "3" << endl;
+    std::memcpy(&byteArray[3], strMsg, numBytes);
+    cout << "4" << endl;
+    std::memcpy(&byteArray[byteArrayIndex - 2], &MESSAGE_END, 4);
+    std::memcpy(&byteArray[byteArrayIndex - 1], &PACKET_END, 4);
+
+    cout << "PACKET CREATED!" << endl;
+
+    for (uint32_t i = 0; i < byteArrayIndex; i++)
+    {
+        printf("WORD AT %d = %08X\n", i, byteArray[i]);
+    }
+    *packet = byteArray;
+    *numBytesRet = (numBytes + HEADER_SIZE);
+    return true;
 }
 
-void ADRV::transmit()
-{
-  size_t nbytes_tx = 0;
-  nbytes_tx = iio_buffer_push(txBuf);
-	cout << "BYTES PUSHED: " << nbytes_tx << endl;
-}
+bool writePacketToBuffer(uint32_t* packet, uint32_t numBytes);
+bool readBufferToPacket(uint32_t* packet, uint32_t* numBytesRet);
 
-void ADRV::receive()
-{
-  size_t nbytes_rx = 0;
-  nbytes_tx = iio_buffer_refill(rxBuf);
-  cout << "BYTES RECEIVED: " << nbytes_rx << endl;
-}
-
-void ADRV::readBufferToPacket(uint8_t* packet, uint32_t &numBytes)
-{
-  cout << "READ: BUFFER TO PACKET" << endl;
-  int i = 0;
-  for (uint8_t* ptr = (uint8_t*)iio_buffer_first(rxBuf, rx0_i); ptr < iio_buffer_end(rxBuf) && i < 32; ptr += iio_buffer_step(rxBuf))
-  {
-    //cout << "BUF at i = " << i << ": " << *ptr << endl;
-    packet[i] = *ptr;
-    i++;
-  }
-  numBytes = i; 
-  cout << "READING COMPLETE" << endl;
-}
-
-
-
-bool createPacket(const char* strMsg, uint32_t numBytes, uint8_t** packet, uint32_t &numBytesRet)
-{
-  uint8_t* byteArray = (uint8_t*)malloc(numBytes + HEADER_SIZE);
-  uint32_t byteArrayIndex = (numBytes + HEADER_SIZE) / sizeof(uint8_t);
-
-  cout << "NUM ELEMENTS: " << byteArrayIndex << endl;
-  cout << "CREATING PACKET..." << endl;
-  std::memcpy(&byteArray[0], &PACKET_START, 4);
-  cout << "1" << endl;
-  std::memcpy(&byteArray[4], &numBytes, 4);
-  cout << "2" << endl;
-  std::memcpy(&byteArray[8], &MESSAGE_START, 4);
-  cout << "3" << endl;
-  std::memcpy(&byteArray[12], strMsg, numBytes);
-  cout << "4" << endl;
-  std::memcpy(&byteArray[byteArrayIndex - 8], &MESSAGE_END, 4);
-  std::memcpy(&byteArray[byteArrayIndex - 4], &PACKET_END, 4);
-
-  cout << "PACKET CREATED!" << endl;
-
-  for (int i = 0; i < byteArrayIndex; i++)
-  {
-    printf("BYTE AT %d = %02X\n", i, byteArray[i]);
-  }
-
-  *packet = byteArray;
-  numBytesRet = byteArrayIndex;
-	return true;
-}
+bool transmit();
+bool receive();
--- ADRV.h
+++ ADRV.h
@@ -2,13 +2,15 @@
 #ifndef ADRV_H
 #define ADRV_H
 
+#include <stdio.h>
+#include <iostream>
 #include <string>
-#include <cstring>
-#include <iomanip>
-#include <stdint.h>
 #include <stdlib.h>
 #include <iio.h>
-#include <stdio.h>
+
+using std::cout;
+using std::endl;
+/* CONSTANTS */
 
 const uint32_t HEADER_SIZE =  20;
 const uint32_t PACKET_START = 0x01535041;
@@ -16,7 +18,7 @@ const uint32_t MESSAGE_START = 0x02434548;
 const uint32_t MESSAGE_END = 0x03415543;
 const uint32_t PACKET_END = 0x04554D4C;
 
-const uint32_t MAX_BUFFER_SIZE = 1024 * 1024;
+const uint32_t MAX_BUFFER_SIZE = 1048576; //1024 * 1024;
 
 const std::string RXDEVICE = "cf-ad9361-lpc";
 const std::string TXDEVICE = "cf-ad9361-dds-core-lpc";
@@ -27,6 +29,14 @@ const std::string ATTR_FS = "sampling_frequency";
 const std::string ATTR_PORT = "rf_port_select";
 const std::string ATTR_F = "frequency";
 
+struct stream_cfg
+{
+  long long bw_hz;
+  long long fs_hz;
+  long long lo_hz;
+  const char* rfport;
+};
+
 enum mode { RXMODE = 0, TXMODE = 1};
 enum cyclicMode { NONCYCLIC = 0, CYCLIC = 1 };
 enum errorCode 
@@ -35,84 +45,47 @@ enum errorCode
   CHN_ERROR, PHYS_ERROR, 
   LO_ERROR, BUF_ERROR
 };
-typedef unsigned long long int gigaInt;
 
-struct stream_cfg
-{
-  gigaInt bw_hz;
-  gigaInt fs_hz;
-  gigaInt lo_hz;
-  const char* rfport;
-};
+/* GLOBALS */
 
-class ADRV
-{
-public:
-  // Default Constructor
-  ADRV();
-  // Parameterized Constructor by field
-  ADRV(gigaInt tx_bw_hz, gigaInt tx_fs_hz, gigaInt tx_lo_hz,
-      gigaInt rx_bw_hz, gigaInt rx_fs_hz, gigaInt rx_lo_hz,
-      const char* tx_port, const char* rx_portName);
-      // Parameterized Constructor by structure
-  ADRV(struct stream_cfg& txCfg, struct stream_cfg& rxCfg);
-  // Default Destructor
-  ~ADRV();
+static struct iio_context* ctx = NULL;
+static struct iio_device* physDev = NULL;
 
-  // Debug the ADRV context
-  void contextHK();
-  // Debug the ADRV's device
-  void deviceHK();
-  // Debug the ADRV's channel
-  void channelHK();
-  // Initialize the receiver module
-  void initializeTransmitter();
+// Transmitter Module
+static struct iio_device* tx = NULL;
+static struct iio_channel* tx0_q = NULL;
+static struct iio_channel* tx0_i = NULL;
+static struct iio_channel* tx_phys = NULL;
+static struct iio_channel* tx_lo = NULL;
+static struct iio_buffer* txBuf = NULL;
+static struct stream_cfg txConfig = NULL;
 
-  void writePacketToBuffer(uint8_t* packet, uint32_t numBytes);
-  void readBufferToPacket(uint8_t* packet, uint32_t &numBytes);
+// Receiver Module
+static struct iio_device* rx = NULL;
+static struct iio_channel* rx0_q = NULL;
+static struct iio_channel* rx0_i = NULL;
+static struct iio_channel* rx_phys = NULL;
+static struct iio_channel* rx_lo = NULL;
+static struct iio_buffer* rxBuf = NULL;
+static struct stream_cfg rxConfig = NULL;
 
-  // Initialize the transmitter module
-  void initializeReceiver();
+void contextHK(static struct iio_context* m_context);
+void deviceHK(static struct iio_device* m_device);
+void channelHK(static struct iio_channel* m_channel);
 
-  // Transmit bufSize bytes from txBuf array - TXPKT used for testing purposes
-  //bool transmit(void* txBuf, uint32_t* TXPKT, uint32_t &bufSize);
-  void transmit();
-  // Receive array rxBuf from the HW, extract the data and write bufSize bytes to RXPKT
-  bool receive();
+bool initADRV();
+bool initTX(static struct stream_cfg* transmitterConfig);
+bool initRX(static struct stream_cfg* receiverConfig);
 
-  void printID();
-  void printObject();
-  void processError(errorCode error, bool debug);
-  bool debugFlag = true;
-private:
+bool createPacket(const char* strMsg, uint32_t numBytes, uint32_t** packet, uint32_t* numBytesRet);
+bool writePacketToBuffer(uint32_t* packet, uint32_t numBytes);
+bool readBufferToPacket(uint32_t* packet, uint32_t* numBytesRet);
 
-  /* ********** IIO CONSTRUCTS **********/
+bool transmit();
+bool receive();
 
-  // Context and internal states
-  struct iio_context* ctx;
-  struct iio_device* physDev;
-  int ADRV_id;
-  int contextDeviceCount;
 
-  // Transmitter Module
-  struct iio_device* tx;
-  struct iio_channel* tx0_q;
-  struct iio_channel* tx0_i;
-  struct iio_channel* tx_phys;
-  struct iio_channel* tx_lo;
-  struct iio_buffer* txBuf;
-  struct stream_cfg txConfig;
 
-  // Receiver Module
-  struct iio_device* rx;
-  struct iio_channel* rx0_q;
-  struct iio_channel* rx0_i;
-  struct iio_channel* rx_phys;
-  struct iio_channel* rx_lo;
-  struct iio_buffer* rxBuf;
-  struct stream_cfg rxConfig;
-};
 
-bool createPacket(const char* strMsg, uint32_t numBytes, uint8_t** packet, uint32_t &numBytesRet);
 
 #endif
