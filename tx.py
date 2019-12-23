74cffe85e1dce782932390b8bc483a4ad7195ab4
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2019-12-23 19:27:10 +0000

    Python TRX test

---
+++ tx.py
@@ -0,0 +1,17 @@
+import adi
+import numpy as np
+
+sdr = adi.ad9364('local:')
+
+
+sdr.sample_rate = int(2.4e6)  # Hz
+sdr.rx_lo = int(1e9)  # Hz
+sdr.tx_lo = int(1e9)  # Hz
+sdr.gain_control_mode = "slow_attack"
+sdr.rx_buffer_size = 16 * 1024
+sdr.dds_enabled = [1, 1, 1, 1]
+sdr.dds_frequencies = [1e6, 1e6, 1e6, 1e6]
+sdr.dds_scales = [1, 1, 0, 0]
+
+while True:
+	sdr.tx([0,1,2,3,4,5,6,7,8,9])
